394c0134921d47072a11b6601bb8f3580ebbc31f
brendanlynn <160085754+brendanlynn@users.noreply.github.com>
2024-07-17 12:09:07 -0400

    Restricted concept Serializable

--- Serializable.h
+++ Serializable.h
@@ -7,7 +7,7 @@ namespace BSerializer {
     concept Serializable = requires(const _T Obj_C, _T Obj_V, const void* Data_C, void* Data_V) {
         { Obj_C.SerializedSize() } -> std::same_as<size_t>;
         { Obj_C.Serialize(Data_V) } -> std::same_as<void>;
-        { _T::Deserialize(Data_C) } -> std::convertible_to<_T>;
+        { _T::Deserialize(Data_C) } -> std::same_as<_T>;
         { _T::Deserialize(Data_C, Obj_V) } -> std::same_as<void>;
     };
 }
