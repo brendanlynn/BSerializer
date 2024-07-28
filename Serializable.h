7b33d4bf5fecc3d730a20a2329459972e4ab0817
brendanlynn <160085754+brendanlynn@users.noreply.github.com>
2024-07-27 20:57:55 -0400

    Documented Serializable.h

--- Serializable.h
+++ Serializable.h
@@ -3,6 +3,27 @@
 #include <concepts>
 
 namespace BSerializer {
+    /**
+     * @brief Concept to check if a type has custom serialization methods.
+     *
+     * A type satisfies BuiltInSerializable if it provides:
+     * - A member function `SerializedSize()` that returns a size_t representing the size of the serialized data.
+     * - A member function `Serialize(void* Data)` that serializes the object to the provided memory location.
+     * - A static member function `Deserialize(const void* Data)` that returns an object of the type from the serialized data.
+     * - A static member function `Deserialize(const void* Data, void* const ObjMem)` that deserializes the data into the provided memory location.
+     *
+     * Example:
+     * @code
+     * struct MyType {
+     *     size_t SerializedSize() const;
+     *     void Serialize(void* Data) const;
+     *     static MyType Deserialize(const void* Data);
+     *     static void Deserialize(const void* Data, void* const ObjMem);
+     * };
+     * @endcode
+     * 
+     * @tparam _T The type whose conformity is evaluated.
+     */
     template <typename _T>
     concept BuiltInSerializable = requires() {
         requires requires (const _T Obj) {
@@ -19,6 +40,37 @@ namespace BSerializer {
         };
     };
 
+    /**
+     * @brief Concept to check if a type is a collection.
+     *
+     * A type satisfies Collection if it provides:
+     * - A nested type `value_type` representing the type of elements in the collection.
+     * - A nested type `size_type` which is an unsigned integral type.
+     * - A nested type `const_iterator` which is an iterator type.
+     * - A member function `size()` that returns the number of elements in the collection.
+     * - Member functions `cbegin()` and `cend()` that return const iterators to the beginning and end of the collection, respectively.
+     * - An iterator can dereference to a const reference of `value_type`.
+     * - An iterator can be incremented with both prefix and postfix operators.
+     * - Iterators can be compared for equality and inequality.
+     * - The collection can be constructed from a pair of pointers to `value_type`.
+     *
+     * Example:
+     * @code
+     * struct MyCollection {
+     *     using value_type = int;
+     *     using size_type = size_t;
+     *     using const_iterator = const int*;
+     *
+     *     size_type size() const;
+     *     const_iterator cbegin() const;
+     *     const_iterator cend() const;
+     *
+     *     MyCollection(const value_type* begin, const value_type* end);
+     * };
+     * @endcode
+     * 
+     * @tparam _T The type whose conformity is evaluated.
+     */
     template <typename _T>
     concept Collection = requires() {
         typename _T::value_type;
@@ -52,6 +104,36 @@ namespace BSerializer {
         };
     };
 
+    /**
+     * @brief Concept to check if a type is a map.
+     *
+     * A type satisfies Map if it:
+     * - Satisfies the Collection concept.
+     * - Provides a nested type `key_type` representing the type of the keys.
+     * - Provides a nested type `mapped_type` representing the type of the values.
+     * - The value_type is a pair of `const key_type` and `mapped_type`.
+     * - Provides a member function `find(const key_type&)` that returns a const iterator to the found element or to the end if not found.
+     *
+     * Example:
+     * @code
+     * struct MyMap {
+     *     using key_type = int;
+     *     using mapped_type = std::string;
+     *     using value_type = std::pair<const key_type, mapped_type>;
+     *     using size_type = size_t;
+     *     using const_iterator = std::map<key_type, mapped_type>::const_iterator;
+     *
+     *     size_type size() const;
+     *     const_iterator cbegin() const;
+     *     const_iterator cend() const;
+     *     const_iterator find(const key_type& key) const;
+     *
+     *     MyMap(const value_type* begin, const value_type* end);
+     * };
+     * @endcode
+     * 
+     * @tparam _T The type whose conformity is evaluated.
+     */
     template <typename _T>
     concept Map = requires() {
         requires Collection<_T>;
@@ -127,27 +209,76 @@ namespace BSerializer {
             > { };
     }
 
+    /**
+     * @brief Concept to check if a type is arithmetic (integral or floating-point).
+     * 
+     * @tparam _T The type whose conformity is evaluated.
+     */
     template <typename _T>
     concept Arithmetic = std::is_arithmetic_v<_T>;
     
+
+    /**
+     * @brief Concept to check if a type is any std::pair<..., ...>.
+     * 
+     * @tparam _T The type whose conformity is evaluated.
+     */
     template <typename _T>
     concept StdPair = details::isStdPair<_T>::value;
 
+    /**
+     * @brief Concept to check if a type is any std::pair<..., ...> and the types of its values are (de)serializable by BSerializer.
+     * 
+     * @tparam _T The type whose conformity is evaluated.
+     */
     template <typename _T>
     concept SerializableStdPair = details::isSerializableStdPair<_T>::value;
 
+    /**
+     * @brief Concept to check if a type is any std::tuple<...>.
+     * 
+     * @tparam _T The type whose conformity is evaluated.
+     */
     template <typename _T>
     concept StdTuple = details::isStdTuple<_T>::value;
 
+    /**
+     * @brief Concept to check if a type is any std::tuple<...> and the types of its values are (de)serializable by BSerializer.
+     * 
+     * @tparam _T The type whose conformity is evaluated.
+     */
     template <typename _T>
     concept SerializableStdTuple = details::isSerializableStdTuple<_T>::value;
 
+    /**
+     * @brief Concept to check if a type is a BSerializer::Collection and the types of its elements are (de)serializable by BSerializer.
+     * 
+     * @tparam _T The type whose conformity is evaluated.
+     */
     template <typename _T>
     concept SerializableCollection = details::isSerializableCollection<_T>::value;
 
+    /**
+     * @brief Concept to check if a type is a BSerializer::Map and the types of its keys and values are (de)serializable by BSerializer.
+     * 
+     * @tparam _T The type whose conformity is evaluated.
+     */
     template <typename _T>
     concept SerializableMap = details::isSerializableMap<_T>::value;
 
+    /**
+     * @brief Concept to check if a type is serializable by BSerializer.
+     * 
+     * A type satisfies Serializable if it conforms to any of the following constraints:
+     * - It satisfies BSerializer::Arithmetic (is integral or floating-point).
+     * - It satisfies BSerializer::SerializableStdPair (is any std::pair<..., ...> and the types of its values are [de]serializable by BSerializer).
+     * - It satisfies BSerializer::SerializableStdTuple (is any std::tuple<...> and the types of its values are [de]serializable by BSerializer).
+     * - It satisfies BSerializer::SerializableCollection (is a BSerializer::Collection and the types of its elements are [de]serializable by BSerializer).
+     * - It satisfies BSerializer::SerializableMap (is a BSerializer::Map and the types of its keys and values are [de]serializable by BSerializer).
+     * - It satisfies BSerializer::BuiltInSerializable (has a preexisting [de]serializer that is compatible with BSerializer).
+     * 
+     * @tparam _T The type whose conformity is evaluated.
+     */
     template <typename _T>
     concept Serializable = details::isSerializable<_T>::value;
 }
